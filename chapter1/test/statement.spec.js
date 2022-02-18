e514b0131ae5476f995c20d886bac9ea2b98452b
thxwelchs <thxwelchs@gmail.com>
2022-02-19 02:54:51 +0900

    [chapter1] 1.1 ~ 1.6 예제 준비

--- chapter1/test/statement.spec.js
+++ chapter1/test/statement.spec.js
@@ -1,10 +1,10 @@
 const assert = require("assert");
-const invoicesData = require("../main/data/invoices.json");
-const playsData = require("../main/data/plays.json");
+const invoicesData = require("./data/invoices.json");
+const playsData = require("./data/plays.json");
 const statement = require("../main/statement.js");
 
 describe("statement.js test", () => {
-  it("영수증 출력", () => {
+  it("BigCo의 청구서는 정상적으로 청구내용이 발행된다", () => {
     const expected = `청구내역 (고객명: BigCo)
 hamlet : $650.00 (55석)
 As You Like It : $580.00 (35석)
@@ -15,4 +15,8 @@ Othello : $500.00 (40석)
 
     assert.strictEqual(expected, statement(invoicesData[0], playsData));
   });
+
+    it("Taehun의 청구서는 지원하지 않는 play.type을 가지고 있어 청구내용 발행이 실패한다", () => {
+        assert.throws(() => statement(invoicesData[1], playsData), Error);
+    });
 });

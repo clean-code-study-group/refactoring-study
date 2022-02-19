5a8574e9e47c0cff2af0785110b1333eeb4884e9
thxwelchs <thxwelchs@gmail.com>
2022-02-19 17:28:23 +0900

    [chapter1] 필요 없어진 매개변수 제거, amountFor()

--- chapter1/main/statement.js
+++ chapter1/main/statement.js
@@ -12,7 +12,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -35,7 +35,7 @@ function statement(invoice, plays) {
     return plays[aPerformance.playID]
   }
 
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
 
     switch (playFor(aPerformance).type) {

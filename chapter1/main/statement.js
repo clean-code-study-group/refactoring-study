937d133d4f6fcf4a5501b8efcbfe83889ae73600
thxwelchs <thxwelchs@gmail.com>
2022-02-19 17:47:18 +0900

    [chapter1] 함수 추출하기, 문장 슬라이드 하기, 추출한 함수 임시 이름 부여하기, totalAmount 계산 하는 부분

--- chapter1/main/statement.js
+++ chapter1/main/statement.js
@@ -2,23 +2,31 @@ const invoicesData = require("./data/invoices.json");
 const playsData = require("./data/plays.json");
 
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
-
   for (let perf of invoice.performances) {
     // 청구 내역을 출력한다.
     result += `${playFor(perf).name} : ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-    totalAmount += amountFor(perf);
   }
+
+  let totalAmount = appleSauce();
 
   result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${(totalVolumeCredits())}점\n`;
 
   return result;
 
+  function appleSauce() {
+    let totalAmount = 0;
+    for (let perf of invoice.performances) {
+      totalAmount += amountFor(perf);
+    }
+
+    return totalAmount;
+  }
+
   function totalVolumeCredits() {
     let volumeCredits = 0;
-    for (let perf of invoicesData[0].performances) {
+    for (let perf of invoice.performances) {
       volumeCredits += volumeCreditsFor(perf);
     }
     return volumeCredits;

da4eef965e03b5a851100658bbad2353b6bd683c
gosuljo <lurutia@naver.com>
2022-02-26 14:51:52 +0900

    [chapter1] amountFor을 중간데이터를 사용하도록 변경

--- chapter1/main/statement.js
+++ chapter1/main/statement.js
@@ -5,7 +5,7 @@ function renderPlainText(data, plays) {
   let result = `청구내역 (고객명: ${data.customer})\n`;
   for (let perf of data.performances) {
     // 청구 내역을 출력한다.
-    result += `${perf.play.name} : ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+    result += `${perf.play.name} : ${usd(perf.amount)} (${perf.audience}석)\n`;
   }
   result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${(totalVolumeCredits())}점\n`;
@@ -15,7 +15,7 @@ function renderPlainText(data, plays) {
   function totalAmount() {
     let totalAmount = 0;
     for (let perf of data.performances) {
-      totalAmount += amountFor(perf);
+      totalAmount += perf.amount;
     }
 
     return totalAmount;

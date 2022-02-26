5d2a5edf4e80d3e07a08a081e362f3cb3b8ccc3e
gosuljo <lurutia@naver.com>
2022-02-26 15:03:49 +0900

    [chapter1] totalAmount, totalVolumeCredits 중간매개체 사용하도록 변경

--- chapter1/main/statement.js
+++ chapter1/main/statement.js
@@ -7,8 +7,8 @@ function renderPlainText(data, plays) {
     // 청구 내역을 출력한다.
     result += `${perf.play.name} : ${usd(perf.amount)} (${perf.audience}석)\n`;
   }
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${(totalVolumeCredits())}점\n`;
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트: ${(data.totalVolumeCredits)}점\n`;
 
   return result;
 

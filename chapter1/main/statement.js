33412cb69f6a24ef54e1d645fd9f2450c4972e15
gosuljo <lurutia@naver.com>
2022-02-26 14:03:51 +0900

    [chapter1] 중간 데이터 구조 추가

--- chapter1/main/statement.js
+++ chapter1/main/statement.js
@@ -1,7 +1,7 @@
 const invoicesData = require("./data/invoices.json");
 const playsData = require("./data/plays.json");
 
-function renderPlainText(invoice, plays) {
+function renderPlainText(data, invoice, plays) {
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
   for (let perf of invoice.performances) {
     // 청구 내역을 출력한다.
@@ -78,7 +78,8 @@ function renderPlainText(invoice, plays) {
 }
 
 function statement(invoice, plays) {
-  return renderPlainText(invoice, plays);
+  const statementData = {};
+  return renderPlainText(statementData, invoice, plays);
 }
 
 console.log(statement(invoicesData[0], playsData));

d6ca2a2f856f76b06a9994d2ca2c7377fd7f502d
gosuljo <lurutia@naver.com>
2022-02-26 14:08:27 +0900

    [chapter1] 고객 데이터를 중간 데이터로 이동

--- chapter1/main/statement.js
+++ chapter1/main/statement.js
@@ -2,7 +2,7 @@ const invoicesData = require("./data/invoices.json");
 const playsData = require("./data/plays.json");
 
 function renderPlainText(data, invoice, plays) {
-  let result = `청구내역 (고객명: ${invoice.customer})\n`;
+  let result = `청구내역 (고객명: ${data.customer})\n`;
   for (let perf of invoice.performances) {
     // 청구 내역을 출력한다.
     result += `${playFor(perf).name} : ${usd(amountFor(perf))} (${perf.audience}석)\n`;
@@ -79,6 +79,7 @@ function renderPlainText(data, invoice, plays) {
 
 function statement(invoice, plays) {
   const statementData = {};
+  statementData.customer = invoice.customer;
   return renderPlainText(statementData, invoice, plays);
 }
 

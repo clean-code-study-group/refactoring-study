db60a17593d710481cdbe0d655717a9d114696b0
gosuljo <lurutia@naver.com>
2022-02-26 15:40:29 +0900

    [chapter1] html 렌더링 함수 추가

--- chapter1/main/statement.js
+++ chapter1/main/statement.js
@@ -22,10 +22,29 @@ function renderPlainText(data, plays) {
   return result;
 }
 
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명 : ${data.customer})</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n";
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+  return result;
+}
+
 function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
 
 console.log(statement(invoicesData[0], playsData));
+// console.log(htmlStatement(invoicesData[0], playsData));
 
 module.exports = statement;

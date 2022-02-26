9a06899445beda5e068969da544fe6ae456a7ed2
gosuljo <lurutia@naver.com>
2022-02-26 14:50:04 +0900

    [chapter1] amountFor statement로 이동

--- chapter1/main/statement.js
+++ chapter1/main/statement.js
@@ -86,11 +86,38 @@ function statement(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
+    result.amount = amountFor(result);
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID]
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+
+    switch (aPerformance.play.type) {
+      case "tragedy": // 비극
+        result = 40000;
+
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy": // 희극
+        result = 30000;
+
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+
+        break;
+      default:
+        throw new Error(`알 수 없는 장르 : ${aPerformance.play.type}`);
+    }
+    return result;
   }
 }
 

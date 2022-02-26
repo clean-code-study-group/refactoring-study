5009a2395c20cf6e71e42ee64d98eb87d575550b
gosuljo <lurutia@naver.com>
2022-02-26 14:26:35 +0900

    [chapter1] 공연 데이터를 얕은복사 하도록 수정

--- chapter1/main/statement.js
+++ chapter1/main/statement.js
@@ -80,8 +80,13 @@ function renderPlainText(data, plays) {
 function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
+  statementData.performances = invoice.performances.map(enrichPerformance);
   return renderPlainText(statementData, plays);
+
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance);
+    return result;
+  }
 }
 
 console.log(statement(invoicesData[0], playsData));

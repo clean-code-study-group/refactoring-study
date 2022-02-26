c6af36ec0e20155f828daf3b17dec2a29f51938d
gosuljo <lurutia@naver.com>
2022-02-26 14:12:03 +0900

    [chapter1] 공연정보 중간 데이터로 이동

--- chapter1/main/statement.js
+++ chapter1/main/statement.js
@@ -1,9 +1,9 @@
 const invoicesData = require("./data/invoices.json");
 const playsData = require("./data/plays.json");
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data, plays) {
   let result = `청구내역 (고객명: ${data.customer})\n`;
-  for (let perf of invoice.performances) {
+  for (let perf of data.performances) {
     // 청구 내역을 출력한다.
     result += `${playFor(perf).name} : ${usd(amountFor(perf))} (${perf.audience}석)\n`;
   }
@@ -14,7 +14,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalAmount() {
     let totalAmount = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       totalAmount += amountFor(perf);
     }
 
@@ -23,7 +23,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalVolumeCredits() {
     let volumeCredits = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       volumeCredits += volumeCreditsFor(perf);
     }
     return volumeCredits;
@@ -80,7 +80,8 @@ function renderPlainText(data, invoice, plays) {
 function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
-  return renderPlainText(statementData, invoice, plays);
+  statementData.performances = invoice.performances;
+  return renderPlainText(statementData, plays);
 }
 
 console.log(statement(invoicesData[0], playsData));

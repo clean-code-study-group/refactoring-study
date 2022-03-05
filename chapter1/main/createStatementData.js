21a63fedf5af4a37920e3e9fdf26390a48501d51
jihyun.park <jihyun.park@jalsalda.com>
2022-03-05 14:11:01 +0900

    [chapter1] 함수 인라인 하기, 공연료 계산 부분 공연료 계산기의 함수 이용

--- chapter1/main/createStatementData.js
+++ chapter1/main/createStatementData.js
@@ -43,7 +43,7 @@ function createStatementData(invoice, plays) {
         const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
-        result.amount = amountFor(result);
+        result.amount = calculator.amount;
         result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
